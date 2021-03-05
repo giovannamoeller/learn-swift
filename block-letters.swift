69a1ad48735a8b6dc0b422da6fa3a891703cd3c3
Giovanna <giovannagmoeller@gmail.com>
2021-03-05 19:07:27 -0300

    Lesson 1 - Hello World

---
+++ block-letters.swift
@@ -0,0 +1,7 @@
+print(" GGG    M   M")
+print("G   G   MM MM")
+print("G       MM MM")
+print("GGGGG   M M M")
+print("G   G   M   M")
+print("G   G   M   M")
+print(" GGG    M   M")
---
+++ hello-world.swift
@@ -0,0 +1,17 @@
+print("Hello, world!")
+
+print("🚙💨")
+
+print("Hello")
+print("Goodbye")
+
+print("       1")
+print("     2 3")
+print("   4 5 6")
+print("7 8 9 10")
+
+// Single Line Comment
+/* Multiple 
+lines
+comment */
+
